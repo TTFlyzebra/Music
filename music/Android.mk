4b5a13fa6ebbdf2993bdf79f599104873225a7de
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-10-10 10:57:18 +0800

    Android原生Music

--- music/Android.mk
+++ music/Android.mk
@@ -3,8 +3,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-	src/com/android/music/IMediaPlaybackService.aidl
+LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
+LOCAL_SRC_FILES := $(call all-java-files-under, src/main/java) \
+    src/com/android/music/IMediaPlaybackService.aidl
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res
+#LOCAL_ASSET_DIR:= $(LOCAL_PATH)/src/main/assets
 
 LOCAL_PACKAGE_NAME := Music
 
